24a5cd76e5acf7c75c987ab7bc912c0f32023894
Ernest Ng <eyn5@cornell.edu>
2024-12-31 21:44:41 -0600

    Accept failing cart test

--- assign8/program/tests/cart-fail/fail1.rs
+++ assign8/program/tests/cart-fail/fail1.rs
@@ -3,6 +3,6 @@ extern crate assign8;
 use assign8::cart::*;
 
 fn main() {
-  // let empty = Cart::login("A".into(), "B".into()).unwrap();
-  // empty.checkout();
+  let empty = Cart::login("A".into(), "B".into()).unwrap();
+  empty.checkout();
 }
